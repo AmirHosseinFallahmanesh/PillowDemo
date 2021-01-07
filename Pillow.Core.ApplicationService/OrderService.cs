48f1fa37c0ed1c7a0d6a3f676fd8d52eb5351631
AmirHosseinFallahmanesh <54832303+AmirHosseinFallahmanesh@users.noreply.github.com>
2021-01-07 17:05:56 +0330

    tesdt

--- Pillow.Core.ApplicationService/OrderService.cs
+++ Pillow.Core.ApplicationService/OrderService.cs
@@ -12,8 +12,6 @@ namespace Pillow.Core.ApplicationService
             this.orderRepository = orderRepository;
         }
 
-      
-
         public void PaymentDone(string token, int tId)
         {
             orderRepository.PaymentDone(token, tId);
